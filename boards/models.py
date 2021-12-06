d7cbd4cc5d390d6c1c6fb9a6a432266ab4001271
HanTengri <muslim39n@gmail.com>
2021-12-06 19:40:21 +0600

    Created Board Models

--- boards/models.py
+++ boards/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from organizations.models import Organization
+from users.models import CustomUser
 
-# Create your models here.
+
+class Board(models.Model):
+    title = models.CharField(max_length=128)
+    description = models.TextField(null=True, blank=True)
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    creator = models.ForeignKey(CustomUser, null=True, on_delete=models.SET_NULL)
+    admins = models.ManyToManyField(CustomUser, related_name='admins')
--- kanban_project/settings.py
+++ kanban_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
     'organizations.apps.OrganizationsConfig',
+    'boards.apps.BoardsConfig',
 ]
 
 MIDDLEWARE = [
